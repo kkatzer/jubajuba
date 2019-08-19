2cdd3dd16cc10144ad06051ebd423c43ceee88d6
Andressa Valengo <valengoandressa@gmail.com>
2019-08-19 11:12:30 -0300

    Revert "Added vector map."
    This reverts commit a69a7d339e78ccf8792bc76b0df0a649ba47c429.

--- Muffin/GameViewController.swift
+++ Muffin/GameViewController.swift
@@ -11,42 +11,47 @@ import SpriteKit
 import GameplayKit
 
 class GameViewController: UIViewController {
-    
-    var sceneNode: GameScene!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        // Load 'GameScene.sks' as a GKScene. This provides gameplay related content
+        // including entities and graphs.
         if let scene = GKScene(fileNamed: "GameScene") {
-        sceneNode = scene.rootNode as! GameScene?
             
-            sceneNode.scaleMode = .aspectFill
-            
-            if let view = self.view as! SKView? {
-                view.presentScene(sceneNode)
+            // Get the SKScene from the loaded GKScene
+            if let sceneNode = scene.rootNode as! GameScene? {
                 
-                view.ignoresSiblingOrder = true
+                // Copy gameplay related content over to the scene
+                sceneNode.entities = scene.entities
+                sceneNode.graphs = scene.graphs
                 
-                view.showsFPS = false
-                view.showsNodeCount = false
+                // Set the scale mode to scale to fit the window
+                sceneNode.scaleMode = .aspectFill
+                
+                // Present the scene
+                if let view = self.view as! SKView? {
+                    view.presentScene(sceneNode)
+                    
+                    view.ignoresSiblingOrder = true
+                    
+                    view.showsFPS = true
+                    view.showsNodeCount = true
+                }
             }
         }
     }
 
-    @IBAction func heldToTheRight(_ sender: Any) {
-        
-    }
-    
-    @IBAction func heldToTheLeft(_ sender: Any) {
-        
-    }
-    
     override var shouldAutorotate: Bool {
         return true
     }
 
     override var supportedInterfaceOrientations: UIInterfaceOrientationMask {
-        return [.landscape, .landscapeLeft/*, .landscapeRight*/]
+        if UIDevice.current.userInterfaceIdiom == .phone {
+            return .allButUpsideDown
+        } else {
+            return .all
+        }
     }
 
     override var prefersStatusBarHidden: Bool {
