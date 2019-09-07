955046a4eb2a9c22eefa789819f10100758b0306
dudamello <duda.mello1@hotmail.com>
2019-09-07 13:14:09 -0300

    fixed gliding

--- Muffin/GameStates/JoyGlidingState.swift
+++ Muffin/GameStates/JoyGlidingState.swift
@@ -34,7 +34,7 @@ class JoyGlidingState: GKState {
         move.water = false
         move.ground = false
         node.removeAllActions()
-        node.run(SKAction.repeatForever(SKAction.animate(with: Animations.Gliding, timePerFrame: 0.022, resize: true, restore: true)), withKey: "gliding")
+        node.run(SKAction.animate(with: Animations.Gliding, timePerFrame: 0.025, resize: true, restore: true))
     }
     
     override func isValidNextState(_ stateClass: AnyClass) -> Bool {
