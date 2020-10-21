6d5684f7c2c2c7e1b2234640daee66a0750cdd18
Vinícius Binder <vinicius.binder@gmail.com>
2020-10-21 16:40:16 -0300

    Fix audio not playing on newer versions of iOS/iPadOS

--- Muffin/AppDelegate.swift
+++ Muffin/AppDelegate.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVKit
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -16,6 +17,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        do {
+            try AVAudioSession.sharedInstance().setCategory(AVAudioSession.Category.playback)
+            try AVAudioSession.sharedInstance().setActive(true)
+        } catch {
+            print("Error: Could not load sound file.")
+        }
+        
         return true
     }
 
